c431c933d062b62e45846abb840ec424d6581b46
Steve Harsant <stevenharsant@gmail.com>
2022-08-01 13:06:02 +0100

    added iscsi watcher service

--- app/README.md
+++ app/README.md
@@ -31,3 +31,13 @@ This Docker compose file is intended for an application/media server. Applicatio
 * Duplicate each template file removing the `.tmpl` extension. Edit the duplicated files with the required environment variables before executing `docker-compose up -d`
 
 * Edit the devices for the Scrutiny container to set the disk paths for your host.
+
+## Helper Scripts
+
+### `iscsi_watcher.sh`
+
+A small utility that takes one (positional) parameter of the directory where an iSCSI device is mounted and ensures its contents are reachable. If they are not, the script restarts the iSCSI daemon via `systemctl`.
+
+A companion service file exists to run this script as a service. It is set to run as `root`. This is not recommended generally and should be changed to a user with only the scope to restart services. The path of the script it starts will likely need to be changed as well to point to the cloned location.
+
+If sleep times or retry thresholds need to be changed, that can be done at the top of the script. (Not parameters as this likely  won't need changing.)
---
+++ app/iscsi_watcher.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+# This script takes in 1 argument, the path to the directory to watch and ensures that the directory is available.
+# If the directory is not available (based on timeout of the ls command on that directory), the script will restart
+# the iSCSI daemon. This is to ensure that the directory is available at all times.
+export ISCSI_WATCHER_DEBUG=true
+debug(){
+  if [ "${ISCSI_WATCHER_DEBUG,,}" == "true" ]; then
+    echo "$1"
+  fi
+}
+
+# Script variables
+directory="$1"; debug "Monitoring $directory"
+pid=32768 # Dummy pid to start the loop
+threshold=3; debug "Attempt threshould is $threshold"
+retest_sleep=0.5; debug "Retest sleep is $retest_sleep"
+success_sleep=10; debug "Success sleep is $success_sleep"
+
+# Start #
+
+while true; do
+  debug "Checking if pid $pid exists"
+
+  if ps -o pid= -p "$pid"; then
+    debug 'pid exists'
+
+    (('i++'))
+    debug "Thread counter incremented to: $i"
+
+    debug "Sleeping for $retest_sleep seconds"
+    sleep $retest_sleep
+
+  else
+    debug 'pid does not exist. Resetting threshould counter'
+    i=1
+
+    debug "Sleeping for $success_sleep seconds"
+    sleep $success_sleep
+
+    debug "Testing directory $directory is accessible"
+    ls "$directory" > /dev/null 2>&1 &
+
+    pid=$!
+    debug "Subprocess pid for test is $pid"
+  fi
+
+  if [ $i -gt $threshold ]; then
+    debug 'Threshold reached'
+
+    if kill -9 "$pid"
+      then debug 'Successfully killed test subprocess'
+      else debug 'Failed to kill test suprocess'
+    fi
+
+    debug "Attempting to restart iSCSI daemon"
+    if systemctl restart iscsid
+      then debug 'Successfully restarted iSCSI daemon. Resetting threshould counter'; i=1
+      else debug 'Failed to restart iSCSI daemon'
+    fi
+
+  fi
+
+done
+
+
+
+
+
+
+
+# done
+
+exit
+
+
+
+
+
+
+# while true; do
+# done
